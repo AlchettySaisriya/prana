d8ef89b62f8b245e9a2dbcdfe4002df21729c047
saisriya <saisriya@123>
2025-04-21 15:29:24 +0000

    initial commit

---
+++ add.java
@@ -0,0 +1,11 @@
+package calculator;
+import java.util.Scanner;
+public class add{
+public void show(){
+Scanner sc=new Scanner(System.in);
+System.out.println("enterthe values");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("addition :" +(a+b));
+}
+}
